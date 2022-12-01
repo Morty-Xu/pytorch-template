20a135fe2847c704f83652963fb0e6af9c454a51
morty <905906821@qq.com>
2022-12-01 22:56:49 +0800

    modify

--- utils/util.py
+++ utils/util.py
@@ -32,8 +32,8 @@ def prepare_device(n_gpu_use):
     """
     n_gpu = torch.cuda.device_count()
     if n_gpu_use > 0 and n_gpu == 0:
-        print("Warning: There\'s no GPU available on this machine,"
-              "training will be performed on CPU.")
+        print("Warning: There\'s no CUDA available on this machine,"
+              "training will be performed on CPU/MPS.")
         n_gpu_use = 0
     if n_gpu_use > n_gpu:
         print(f"Warning: The number of GPU\'s configured to use is {n_gpu_use}, but only {n_gpu} are "
